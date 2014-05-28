6e666046d257cc6ae75fc6c38c6b68b277eb4fa1
Lars Windolf <lars.lindner@gmail.com>
2014-05-29 00:23:00 +0200

    Do proper JSON for node endpoint.

--- dotm-backend/backend.py
+++ dotm-backend/backend.py
@@ -7,14 +7,17 @@ from bottle import route, run
 @route('/nodes')
 def nodes():
 	r = redis.Redis()
-	# FIXME: Add node interconnections to JSON (but missing in Redis currently)
+	# FIXME: Add node interconnections to JSON (but missing in data model currently)
 	return json.dumps(r.lrange("dotm::nodes", 0, -1))
 
 @route('/node/<name>')
 def node(name):
 	r = redis.Redis()
-	services = r.keys('dotm::services::'+name+'*')
-	output = 'name=' + name + '(' + ','.join(services) + ')'
-	return output
+	prefix = 'dotm::services::'+name+'::'
+	services = [s.replace(prefix, '') for s in r.keys(prefix+'*')]
+	# FIXME: Add service details
+	# FIXME: Add connections for this node
+	# FIXME: Add node interconnections for this node (but missing in data model currently)
+	return json.dumps({'name': name, 'services': services})
 
 run(host='localhost', port=8080, debug=True)
