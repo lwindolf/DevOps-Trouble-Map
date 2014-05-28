414a320795358a69de4f2489e3d23b34cb1838dc
Lars Windolf <lars.lindner@gmail.com>
2014-05-28 22:06:01 +0200

    Preparing Python bottle based JSON backend.

---
+++ dotm-backend/backend.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import json
+from bottle import route, run
+from dotm_redis import DOTMRedis
+
+@route('/nodes')
+def nodes():
+	redis = DOTMRedis('mon')
+	# FIXME: Add node interconnections to JSON (but missing in Redis currently)
+	return json.dumps(redis.lrange("dotm::nodes"))
+
+@route('/node/<name>')
+def node(name):
+	redis = DOTMRedis('mon')
+	services = redis.keys('dotm::services::'+name+'*')
+	output = 'name=' + name + '(' + ','.join(services) + ')'
+	return output
+
+run(host='localhost', port=8080, debug=True)
