ed1b2762c94c7e15f3281c1b15fa3d89fa2d87ca
anryko <andrej.svenke@deliveryhero.com>
2014-05-30 11:15:58 +0200

    Updated Monitoring API POC

--- dotm-monitor/dotm_mon/dotm_mon.py
+++ dotm-monitor/dotm_mon/dotm_mon.py
@@ -4,14 +4,14 @@ import json
 
 class DOTMMonitor:
 
-	version = '0.1.2'
+	version = '0.1.3'
 
 	def __init__(self, mon_url, user=None, paswd=None, provider='icinga'):
 		self.user = user
 		self.paswd = paswd
 		self.provider = provider
 		if provider == 'icinga':
-			self.mon_url = mon_url.rstrip('/') + '/cgi-bin/icinga/status.cgi?&style=detail&jsonoutput'
+			self.mon_url = mon_url.rstrip('/') + '/cgi-bin/icinga/status.cgi?style=hostservicedetail&jsonoutput'
 		elif provider == 'nagios':
 			#TODO: implement more providers for Monitoring.mon_url
 			pass
@@ -27,7 +27,21 @@ class DOTMMonitor:
 	def get_data(self):
 		return self._get_req().text
 
-	def _get_json_icinga(self):
+	def _get_hosts_icinga(self):
+		data = self.get_data()
+		jsonData = json.loads(data.replace('\t', ' '))
+		js = jsonData.get('status').get('host_status')
+		rjs = {}
+		for elem in js:
+			rjs[elem['host']] = {
+				'status': elem['status'],
+				'last_check': elem['last_check'],
+				'duration': elem['duration'],
+				'status_information': elem['status_information']
+			}
+		return rjs
+
+	def _get_services_icinga(self):
 		data = self.get_data()
 		jsonData = json.loads(data.replace('\t', ' '))
 		js = jsonData.get('status').get('service_status')
@@ -45,7 +59,36 @@ class DOTMMonitor:
 			}
 		return rjs
 
-	def get_json(self):
+	def get_hosts(self):
+		"""
+		Return json format:
+
+		{
+			"hostname01": {
+				"status": "UP",
+				"last_check": "<timestamp>",
+				"duration": "<nagios duration format>", #FIXME: figure out the way to unify it
+				"status_information": "hostname01 status information"
+			},
+			"hostname02": {
+				"status": "DOWN",
+				"last_check": "<timestamp>",
+				"duration": "<nagios duration format>",
+				"status_information": "hostname01 status information"
+			},
+			.
+			.
+			.
+		}
+		"""
+		if self.provider == 'icinga':
+			return self._get_hosts_icinga()
+		elif provider == 'nagios':
+			#TODO: implement more providers for Monitoring.get_hosts()
+			pass
+
+
+	def get_services(self):
 		"""
 		Returned json format:
 
@@ -53,14 +96,14 @@ class DOTMMonitor:
 			"hostname01": {
 				"Service01 name": {
 					"status": "OK",
-					"last_check": "<timedate>",
-					"duration": "<nagios guration format>", #FIXME: figure out the way to unify it
+					"last_check": "<timestamp>",
+					"duration": "<nagios duration format>", #FIXME: figure out the way to unify it
 					"status_information": "Service01 status information"
 				},
 				"Service02 name": {
 					"status": "CRITICAL",
-					"last_check": "<timedate>",
-					"duration": "<nagios guration format>",
+					"last_check": "<timestamp>",
+					"duration": "<nagios duration format>",
 					"status_information": "Service02 status information"
 				},
 			},
@@ -75,8 +118,8 @@ class DOTMMonitor:
 		}
 		"""
 		if self.provider == 'icinga':
-			return self._get_json_icinga()
+			return self._get_services_icinga()
 		elif provider == 'nagios':
-			#TODO: implement more providers for Monitoring.get_json()
+			#TODO: implement more providers for Monitoring.get_services()
 			pass
 
