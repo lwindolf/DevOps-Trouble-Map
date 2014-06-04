31eb9ef03b314c147b5f000eb8d912b2a621ffe1
Lars Windolf <lars.lindner@gmail.com>
2014-06-04 23:34:11 +0200

    Fix typo

---
+++ scripts/install-server.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This is a simple DOTM installer for Debian/Ubuntu
+
+readonly PREFIX=${1-/usr/local}
+
+if [ ! -f configure.ac ]; then
+	echo "ERROR: Please run this script from source root!"
+	exit 1
+fi
+
+if ! lsb_release -i | egrep 'Debian|Ubuntu' >/dev/null; then
+	echo "ERROR: Currently only Debian/Ubuntu are supported by this script!"
+	echo "Please contribute installation instructions for other distributions!"
+	exit 1
+fi
+	
+echo "### Installing dependencies..."
+apt-get install netcat redis-server python-redis python-bottle python-requests python-configparser apache2
+
+echo "### Compiling and installing ($PREFIX)..."
+autoreconf -i
+./configure --prefix=$PREFIX
+make clean && make && make install
+if [ $? -ne 0 ]; then
+	echo "ERROR: Compiling/installation failed!"
+	exit 1
+fi
+ 
+echo "### Installing backend init script..."
+cp dotm_backend/dotm_backend.rc /etc/init.d/dotm_backend
+chmod a+x /etc/init.d/dotm_backend
+update-rc.d dotm_backend defaults
+/etc/init.d/dotm_backend start
+
+echo "### Setting up Apache config..."
+ln -s /usr/local/share/dotm_frontend/apache-2.4.conf /etc/apache2/conf-enabled/dotm.conf
+/etc/init.d/apache2 reload
