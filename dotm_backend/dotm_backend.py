7a3f94d52bf650da5c5928cb8fad9bdfa09f115f
anryko <andrej.svenke@deliveryhero.com>
2014-06-14 07:10:50 +0200

    Ported backend to python3 and improved get_connections()

--- dotm_backend/dotm_backend.py
+++ dotm_backend/dotm_backend.py
@@ -1,54 +1,57 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import re
 import json
 import redis
-import datetime
 from bottle import route, run, response
 
-def get_connections(r):
-	prefix = 'dotm::connections::'
-	tmp = [re.sub('::[0-9]+::', '::', k.replace(prefix, '')) for k in r.keys(prefix+'*')]
-	keys = []
-	for t in sorted(set(tmp)):
-		fields = t.split('::')
-		if len(fields) == 2:
-			# Filter out local host addresses
-			if not (fields[0].startswith('127') or fields[1].startswith('127')):
-				keys.append({'source': fields[0], 'destination': fields[1]})
-	return keys
+rdb = redis.Redis(decode_responses=True)
+
+
+def get_connections():
+    prefix = 'dotm::connections::'
+    key_arr = []
+    for key in rdb.keys(prefix + '*'):
+        field_arr = key.split('::')
+        if not (field_arr[3].isdigit() or field_arr[4].startswith('127')
+                or field_arr[2].startswith('127')):
+            key_arr.append({'source': field_arr[2], 'destination': field_arr[4]})
+    return key_arr
+
 
 @route('/nodes')
 def nodes():
-	response.set_header('Cache-Control', 'max-age=30,must-revalidate')
-	r = redis.Redis()
-	return json.dumps({'nodes':r.lrange("dotm::nodes", 0, -1), 'connections': get_connections(r)})
+    response.set_header('Cache-Control', 'max-age=30,must-revalidate')
+    response.content_type = 'application/json'
+    return json.dumps({'nodes': rdb.lrange("dotm::nodes", 0, -1),
+                       'connections': get_connections()})
+
 
 @route('/node/<name>')
 def node(name):
-	response.set_header('Cache-Control', 'max-age=30,must-revalidate')
-	r = redis.Redis()
+    response.set_header('Cache-Control', 'max-age=30,must-revalidate')
+    response.content_type = 'application/json'
+    prefix = 'dotm::nodes::' + name
+    nodeDetails = rdb.hgetall(prefix)
 
-	prefix = 'dotm::nodes::'+name
-	nodeDetails = r.hgetall(prefix)
+    prefix = 'dotm::services::' + name + '::'
+    serviceDetails = {}
+    services = [s.replace(prefix, '') for s in rdb.keys(prefix + '*')]
+    for s in services:
+        serviceDetails[s] = rdb.hgetall(prefix + s)
 
-	prefix = 'dotm::services::'+name+'::'
-	serviceDetails = {}
-	services = [s.replace(prefix, '') for s in r.keys(prefix+'*')]
-	for s in services:
-		serviceDetails[s] = r.hgetall(prefix+s)  
+    prefix = 'dotm::connections::' + name + '::'
+    connectionDetails = {}
+    connections = [c.replace(prefix, '') for c in rdb.keys(prefix + '*')]
+    for c in connections:
+        tmp = c.split('::')
+        if len(tmp) == 2:
+            cHash = rdb.hgetall(prefix + c)
+            cHash['localPort'] = tmp[0]
+            cHash['remoteHost'] = tmp[1]
+            connectionDetails[c] = cHash
 
-	prefix = 'dotm::connections::'+name+'::'
-	connectionDetails = {}
-	connections = [c.replace(prefix, '') for c in r.keys(prefix+'*')]
-	for c in connections:
-		tmp = c.split('::')
-		if len(tmp) == 2:
-			cHash = r.hgetall(prefix+c)
-			cHash['localPort'] = tmp[0]
-			cHash['remoteHost'] = tmp[1]
-			connectionDetails[c] = cHash
+    return json.dumps({'name': name, 'status': nodeDetails, 'services': serviceDetails,
+                       'connections': connectionDetails})
 
-	return json.dumps({'name': name, 'status': nodeDetails, 'services': serviceDetails, 'connections': connectionDetails})
-
-run(host='localhost', port=8080, debug=True)
+if __name__ == '__main__':
+    run(host='localhost', port=8080, reloader=True, debug=True)
