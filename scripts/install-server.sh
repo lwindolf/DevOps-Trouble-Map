3862c9eaf47978922b8334a498c35e366115d61d
Lars Windolf <lars.windolf@gmx.de>
2014-07-25 11:26:01 +0200

    Ensure we have curl (used by /mon/reload cron)

--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -16,7 +16,7 @@ if ! lsb_release -i | egrep 'Debian|Ubuntu' >/dev/null; then
 fi
 	
 echo "### Installing dependencies..."
-apt-get install netcat redis-server python-redis python-bottle python-requests python-geoip geoip-database
+apt-get install netcat redis-server python-redis python-bottle python-requests python-geoip geoip-database curl
 
 echo "### Compiling and installing ($PREFIX)..."
 autoreconf -i
