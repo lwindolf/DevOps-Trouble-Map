23d4e9aae10e769503a2e8116255791086a574c0
Lars Windolf <lars.windolf@gmx.de>
2014-07-29 12:23:48 +0200

    Merge service status for gmap view bubbles. Fixes bubble icon for red. Show faulty services in bubble popup.

--- frontend/static/dotm_view_gmap.js
+++ frontend/static/dotm_view_gmap.js
@@ -1,3 +1,20 @@
+function dotm_view_gmap_render_services (data) {
+        if (!data)
+                return "";
+
+        if (!data['services'])
+                return "";
+
+        var result = "";
+        for (var i = 0; i < data['services'].length; i++) {
+		var s = data['services'][i];
+		if (s['status'] != 'OK')
+	                result += " <span class='service status_"+s['status']+"'> " + s['service'] + " </span>";
+        }
+
+        return result;
+}
+
 function loadGMap(stage, locations) {
 	// Clean everything
 	clearStatus(stage);
@@ -6,12 +23,20 @@ function loadGMap(stage, locations) {
 	// Add marker icons to locations list
 	$.each(locations, function(index, ip) {
 		try {
+			/* Merge maximum node and service status */
 			var status = ip.data.monitoring.node.status;
+			$.each(ip.data.monitoring.services, function(index, s) {
+				if(s['status'] == 'WARNING' && status != "DOWN")
+					status = s['status'];
+				if(s['status'] == 'CRITICAL')
+					status = "DOWN";
+			});
+
 			ip['options'] = new Array();
 			if(status == 'UP')
 				ip['options']['icon'] = 'http://maps.google.com/mapfiles/marker_green.png';
 			else if(status == 'DOWN')
-				ip['options']['icon'] = 'http://maps.google.com/mapfiles/marker_red.png';
+				ip['options']['icon'] = 'http://maps.google.com/mapfiles/marker.png';
 			else
 				ip['options']['icon'] = 'http://maps.google.com/mapfiles/marker_orange.png';
 
@@ -42,7 +67,10 @@ function loadGMap(stage, locations) {
 			    latLng: marker.getPosition(),
 			    options:{
 			      content:  "<div class='mapbullet'>" +
-				          "<div class='text'>" + context.data.node + " (" + context.data.ip + ")</div>" +
+				          "<div class='text'>" + context.data.node + 
+                                          " (" + context.data.ip + ")" +
+                                          dotm_view_gmap_render_services (context.data['monitoring']) +
+					  "</div>" +
 				        "</div>",
 			      offset: {
 				x:-46,
