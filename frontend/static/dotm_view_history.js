9396177ae65a3bda09cc8fa1987bd8380238d75f
Lars Windolf <lars.windolf@gmx.de>
2014-08-15 22:38:23 +0200

    Visual snapshot list navigation and proper forward and backward button disabling.

--- frontend/static/dotm_view_history.js
+++ frontend/static/dotm_view_history.js
@@ -13,16 +13,20 @@ DOTMViewHistory.prototype.reload = function() {
 
 	$.getJSON("backend/history", {})
 	.done(function (data) {
-		var navigation, list;
+		var navigation, list = "";
 		var selected = getHistoryIndex();
 		var selectedIndex = -1;
 		
-		list = "<h3>List of Snapshots</h3><div class='history'>";
-		$.each(data, function(index, timestamp) {
-			list += "<div class='snapshot'><a href='javascript:setHistoryIndex(\""+timestamp+"\")'>"+timestamp+"</a><div>";
+		$.each(data.sort(), function(index, timestamp) {
 			if(selected != "" && selected == timestamp)
 				selectedIndex = index;
+
+			list = "<div id='snapshot_"+timestamp+"' class='snapshot'>"+
+				"<a class='"+((selectedIndex == index)?" selected":"")+"' href='javascript:setHistoryIndex(\""+timestamp+"\")'>"+
+				new Date(timestamp*1000).toString()+
+				"</a></div>" + list;
 		});
+		list = "<h3>List of Snapshots</h3><div class='history'>" + list;
 		list += "</div></div>";
 
 		if(selected != "" && selectedIndex == -1)
@@ -37,14 +41,17 @@ DOTMViewHistory.prototype.reload = function() {
 				"<p>" +
 				"<input type='button' value='Reset to Live View' onclick='setHistoryIndex(\"\")'/>" +
 				"</p><p>" +
-				"<input type='button' value='&lt;&lt;&lt;' title='Previous Snapshot' onclick='setHistoryIndex(\""+data[selectedIndex - 1]+"\")'/>"+
-				"<input type='button' value='>>>' title='Next Snapshot' onclick='setHistoryIndex(\""+data[selectedIndex + 1]+"\")'/>"+
+				"<input type='button' value='&lt;&lt;&lt;' title='More Recent Snapshot' onclick='setHistoryIndex(\""+data[selectedIndex + 1]+"\")'"+
+				((selectedIndex + 1 >= data.length)?" disabled":"")+"/>"+
+				"<input type='button' value='>>>' title='Older Snapshot' onclick='setHistoryIndex(\""+data[selectedIndex - 1]+"\")'"+
+				((selectedIndex - 1 < 0)?" disabled":"")+"/>"+
 				"</p>";
 		else
 			navigation += "<p>Select a snapshot to view a past system state.</p>";
 
 		$(view.stage).html(navigation + list);
 		clearStatus(view.stage);
+		$(".snapshot a.selected").css("background", "#ccc");
 	})
 	.fail(function (jqxhr, textStatus, error) {
 		setError(view.stage, 'Fetching snapshot list failed! ('+error+')');
