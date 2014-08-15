d4fc60305a616830331cbf04ed6ae5e4be1fff85
Lars Windolf <lars.lindner@gmail.com>
2014-08-15 18:23:07 +0200

    Some namespace keys should not be history prefixed.

--- backend/dotm_namespace.py
+++ backend/dotm_namespace.py
@@ -8,6 +8,13 @@ class DOTMNamespace(object):
 
     def __init__(self, history_key=None):
         self.prefix = 'dotm'
+
+        # Some things should never be history prefixed as we always want to 
+        # access the current values
+        self.queue = self.prefix + '::queue'
+        self.history = self.prefix + '::history'
+        self.config = self.prefix + '::config'
+
         if history_key:
             self.history_prefix = history_key + '::' + self.prefix
         else:
@@ -16,12 +23,8 @@ class DOTMNamespace(object):
         self.nodes = self.history_prefix + '::nodes'
         self.connections = self.history_prefix + '::connections'
         self.services = self.history_prefix + '::services'
-        self.config = self.history_prefix + '::config'
         self.resolver = self.history_prefix + '::resolver'
 
         self.checks = self.history_prefix + '::checks'
         self.nodes_checks = self.checks + '::nodes'
         self.services_checks = self.checks + '::services'
-
-        self.queue = self.prefix + '::queue'
-        self.history = self.prefix + '::history'
