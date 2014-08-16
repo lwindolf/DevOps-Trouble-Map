5ed048b0068ff3a46fc3e7ca097daea80a952202
Lars Windolf <lars.lindner@gmail.com>
2014-08-16 13:24:33 +0200

    Added problem report view. (Github #51)

---
+++ frontend/static/dotm_view_report.js
@@ -0,0 +1,38 @@
+/* Simple view that prints an problem report table */
+
+function DOTMViewReport(stage) {
+	this.stage = stage;
+}
+
+DOTMViewReport.prototype.reload = function() {
+	var view = this;
+
+	// Clean everything
+	setStatus(this.stage, 'Fetching report...');
+	$(this.stage).html("test");
+
+	$.getJSON("backend/report", {})
+	.done(function (data) {
+		try {
+			var list = "";
+			$.each(data.alerts, function(node, alerts) {
+				$.each(alerts, function(index, alert) {
+					list += "<tr class='service'>" +
+						"<td class='category'>"+alert['category']+"</td>"+
+						"<td class='node'>"+node+"</td>"+
+						"<td class='service severity status_"+alert['severity']+"'>"+alert['severity']+"</td>"+
+						"<td class='message'>"+alert['message']+"</td>"+
+						"</tr>";
+				});
+			});
+			list = "<div class='settingsForm'><h3>Problem Report</h3><table id='report' border='1'>" + list + "</table></div>";
+			$(view.stage).html(list);
+			clearStatus(view.stage);
+		} catch(e) {
+			setError(view.stage, 'Rendering report failed! ('+e+')');
+		}
+	})
+	.fail(function (jqxhr, textStatus, error) {
+		setError(view.stage, 'Fetching report failed! ('+error+')');
+	})
+}
